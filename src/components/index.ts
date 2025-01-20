21ef9e03d28252df9389f7a0543ab641a180edbb
Sauli Anto <sauli.anto@gofore.com>
2025-01-20 13:24:57 +0200

    OPHJOD-1194: Use NavigationBar from design system

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,26 +1,5 @@
 export { LanguageButton } from './LanguageButton/LanguageButton';
 export { LanguageMenu } from './LanguageMenu/LanguageMenu';
-export {
-  LogoArrowBlack,
-  LogoArrowBlue,
-  LogoArrowOrange,
-  LogoArrowPink,
-  LogoArrowTurquoise,
-  LogoArrowWhite,
-  LogoBlackEn,
-  LogoBlackFi,
-  LogoBlackSv,
-  LogoIconBlack,
-  LogoIconRgb,
-  LogoIconWhite,
-  LogoRgbEn,
-  LogoRgbFi,
-  LogoRgbSv,
-  LogoWhiteEn,
-  LogoWhiteFi,
-  LogoWhiteSv,
-} from './Logo';
 export { MainLayout } from './MainLayout/MainLayout';
-export { NavigationBar } from './NavigationBar/NavigationBar';
 export { Title } from './Title/Title';
 export { UserButton } from './UserButton/UserButton';
