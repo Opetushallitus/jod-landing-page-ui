b910bdfc1e2339ccbacde1235a0adc74d22e5576
Sauli Anto <sauli.anto@gofore.com>
2025-01-24 14:59:44 +0200

    OPHJOD-1226: Update Tailwind CSS to version 4

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   test: {
     environment: 'jsdom',
     globals: true,
