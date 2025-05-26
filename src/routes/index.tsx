e7167de0e60a7cc3990dc97b34f7ad0b7b5902a5
Sami Korpela <sami.korpela@gofore.com>
2025-05-26 09:04:10 +0300

    OPHJOD-1618: Add language selection to navigationbar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,12 @@ import i18n from '@/i18n/config';
 import { RouteObject, replace } from 'react-router';
 import { Home } from './Home';
 import { NoMatch, Root } from './Root';
+import loader from './Root/loader';
 
 const rootRoute: RouteObject = {
   id: 'root',
   path: '/:lng',
+  loader: loader,
   element: <Root />,
   children: [
     {
