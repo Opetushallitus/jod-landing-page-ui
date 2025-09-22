fa061fe73b5e5c02b40a7a9ada6f23bac72e9790
Sauli Anto <sauli.anto@gofore.com>
2025-09-22 15:28:27 +0300

    OPHJOD-2118: Add basic information pages

---
+++ src/routes/BasicInformation/index.ts
@@ -0,0 +1,7 @@
+import AboutService from './AboutService';
+import AiUsage from './AiUsage';
+import BasicInformation from './BasicInformation';
+import DataSources from './DataSources';
+import PrivacyAndCookies from './PrivacyAndCookies';
+
+export { AboutService, AiUsage, BasicInformation, DataSources, PrivacyAndCookies };
