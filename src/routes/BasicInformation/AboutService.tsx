7c5821d16f8942b59544ca4cd6cba27f0675dbda
Sauli Anto <sauli.anto@gofore.com>
2025-09-24 19:20:21 +0300

    OPHJOD-2201: Add about service

--- src/routes/BasicInformation/AboutService.tsx
+++ src/routes/BasicInformation/AboutService.tsx
@@ -1,33 +1,74 @@
+import { IconHeading } from '@/components/IconHeading';
 import { MainLayout } from '@/components/MainLayout/MainLayout';
+import { ScrollHeading } from '@/components/ScrollHeading/ScrollHeading';
+import { getLinkTo } from '@/utils/routeUtils';
 import { MenuSection, PageNavigation } from '@jod/design-system';
+import { JodInfo } from '@jod/design-system/icons';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { ArticleSection } from '../types';
 
 const AboutService = () => {
   const { t } = useTranslation();
   const title = t('about-service.title');
 
+  const sections: ArticleSection[] = React.useMemo(() => {
+    return (
+      t('about-service.sections', { returnObjects: true }) as unknown as {
+        navTitle: string;
+        paragraphs: string[];
+      }[]
+    ).map((item) => {
+      const { navTitle, paragraphs } = item;
+
+      return {
+        navTitle,
+        content: (
+          <div>
+            {paragraphs.map((p) => (
+              <p key={p.slice(0, 25)} className="mb-4 last:mb-0">
+                {p}
+              </p>
+            ))}
+          </div>
+        ),
+      };
+    });
+  }, [t]);
+
   const navChildren = React.useMemo(() => {
     const menuSection: MenuSection = {
       title: t('on-this-page'),
-      linkItems: [],
+      linkItems: sections.map((section) => ({
+        label: section.navTitle,
+        LinkComponent: getLinkTo(`#${section.navTitle}`),
+      })),
     };
     return <PageNavigation menuSection={menuSection} activeIndicator="dot" className="mb-4" />;
-  }, [t]);
+  }, [t, sections]);
 
   return (
     <MainLayout navChildren={navChildren}>
       <title>{title}</title>
-      <h1 data-testid="about-service-title" className="mb-5 text-heading-2 sm:text-heading-1">
-        {title}
-      </h1>
-      <p className="mb-8 text-body-md font-arial">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-        laborum.
-      </p>
+
+      <IconHeading icon={<JodInfo />} title={title} dataTestId="about-service-title" />
+
+      <div className="font-arial">
+        <div className="flex flex-col mb-7">
+          <p className="text-body-lg-mobile sm:text-body-lg">{t('about-service.intro')}</p>
+        </div>
+
+        {sections.map((section) => (
+          <div key={section.navTitle} className="flex flex-col mb-7">
+            <ScrollHeading
+              title={section.navTitle}
+              heading="h2"
+              className="text-heading-2-mobile sm:text-heading-2 font-poppins mb-3"
+            />
+            <div className="flex flex-row justify-between">{section.content}</div>
+          </div>
+        ))}
+      </div>
     </MainLayout>
   );
 };
