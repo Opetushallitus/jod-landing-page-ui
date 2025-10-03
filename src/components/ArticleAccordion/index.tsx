7ed84aedda2999b98ae51fb8adf4c9daf8ffa333
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2025-10-03 13:42:25 +0300

    OPHJOD-2367: Update DS and fix Accodion props

--- src/components/ArticleAccordion/index.tsx
+++ src/components/ArticleAccordion/index.tsx
@@ -1,12 +1,8 @@
 import { Accordion } from '@jod/design-system';
-import { useTranslation } from 'react-i18next';
 
 export const ArticleAccordion = ({ titleText, content }: { titleText: string; content: React.ReactNode }) => {
-  const {
-    i18n: { language },
-  } = useTranslation();
   return (
-    <Accordion title={titleText} lang={language} initialState={false} className="bg-bg-gray-2 p-3">
+    <Accordion title={titleText} initialState={false} className="bg-bg-gray-2 p-3">
       <div className="px-5 pb-3">{content}</div>
     </Accordion>
   );
