867cdb0cfdf28414c8cbb4f2307cd77a18a6f5a8
Sami Korpela <sami.korpela@gofore.com>
2026-01-22 17:29:11 +0200

    OPHJOD-3049: Add logos to replace texts

---
+++ src/components/Logos/index.ts
@@ -0,0 +1,3 @@
+export { LogoOpinfi } from './opinfi';
+export { LogoOpintopolkuEn, LogoOpintopolkuFi, LogoOpintopolkuSv } from './opintopolku';
+export { LogoTyomarkkinatoriEn, LogoTyomarkkinatoriFi, LogoTyomarkkinatoriSv } from './tyomarkkinatori';
