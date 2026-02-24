e48673d3a65596dada274e718392443440c51a96
Sauli Anto <sauli.anto@gofore.com>
2026-02-24 14:14:33 +0200

    OPHJOD-3162: Add ellipsis and caretPosition props

--- src/components/ArticleAccordion/index.tsx
+++ src/components/ArticleAccordion/index.tsx
@@ -2,7 +2,7 @@ import { Accordion } from '@jod/design-system';
 
 export const ArticleAccordion = ({ titleText, content }: { titleText: string; content: React.ReactNode }) => {
   return (
-    <Accordion title={titleText} initialState={false} className="bg-bg-gray-2 p-3">
+    <Accordion title={titleText} initialState={false} className="bg-bg-gray-2 p-3" ellipsis={false} caretPosition="top">
       <div className="px-5 pb-3">{content}</div>
     </Accordion>
   );
