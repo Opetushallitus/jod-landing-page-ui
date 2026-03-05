ca9c63af8b6a2fe137739010ba0e89e49f3a6372
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2026-03-05 09:48:10 +0200

    OPHJOD-3167: Update Home page to match latest design

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -11,10 +11,9 @@ import {
   LogoTyomarkkinatoriFi,
   LogoTyomarkkinatoriSv,
 } from '@/components/Logos';
-import { getLinkTo } from '@/utils/routeUtils';
-import { Button, cx, HeroCard, tidyClasses as tc, useMediaQueries } from '@jod/design-system';
-import { JodOpenInNew } from '@jod/design-system/icons';
-import React, { JSX } from 'react';
+import { Accordion, HeroCard, tidyClasses as tc, useMediaQueries } from '@jod/design-system';
+import { JodBuild, JodCertificate, JodFavs, JodOpenInNew, JodQuestionMark } from '@jod/design-system/icons';
+import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { Link } from 'react-router';
 
@@ -82,7 +81,7 @@ const MainCard = () => {
       <div className="mx-auto max-w-[1092px] px-5 sm:px-6 xl:px-0 relative" ref={firstCardRef}>
         <div className="max-w-[716px]">
           <HeroCard
-            backgroundColor="var(--color-secondary-1-dark)"
+            backgroundColor="var(--color-secondary-1-dark-2)"
             content={t('home.hero.content')}
             title={t('home.hero.title')}
             titleLevel={1}
@@ -97,117 +96,23 @@ const MainCard = () => {
   );
 };
 
-const SecondaryCard = ({
-  title,
-  content,
-  color,
-  buttonIcon,
-  buttonLabel,
-  to,
-  bgImageClassName,
-}: {
-  title: string;
-  content: string;
-  color: string;
-  buttonIcon?: JSX.Element;
-  buttonLabel: string;
-  to: string;
-  bgImageClassName: string;
-}) => {
-  return (
-    <div className={`h-auto ${bgImageClassName} py-8`}>
-      <div className="max-w-[1092px] mx-auto px-5 sm:px-6 xl:px-0">
-        <div className="max-w-[716px]">
-          <HeroCard
-            size="sm"
-            backgroundColor={color}
-            content={content}
-            title={title}
-            to={to}
-            linkComponent={ExternalLink}
-            buttonLabel={buttonLabel}
-            buttonIcon={buttonIcon}
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-const ServiceBlock = ({
-  title,
-  text,
-  buttonLabel,
-  to,
-  className,
-}: {
-  title: string;
-  text: string;
-  buttonLabel: string;
-  to: string;
-  className?: string;
-}) => {
-  const { t } = useTranslation();
-  return (
-    <div className="w-full h-full flex flex-col justify-between">
-      <div>
-        <h3 className="text-heading-3-mobile sm:text-heading-3 mb-5">{title}</h3>
-        <p className="text-body-sm-mobile sm:text-body-sm mb-6">{text}</p>
-      </div>
-      <div>
-        <Button
-          className={cx('active:hover:underline', 'group-focus:underline', className)}
-          variant="accent"
-          label={buttonLabel}
-          icon={<JodOpenInNew ariaLabel={t('common:external-link')} />}
-          iconSide="right"
-          linkComponent={getLinkTo(to, {
-            useAnchor: true,
-            target: '_blank',
-          })}
-        />
-      </div>
-    </div>
-  );
-};
-
-const Block = ({
-  title,
-  content,
-  children,
-  isLast,
-}: {
-  title: string;
-  content?: string;
-  children?: JSX.Element;
-  isLast?: boolean;
-}) => {
-  return (
-    <div
-      className={`sm:pt-5 mt-11 px-5 sm:px-6 xl:px-0 max-w-[1092px] mx-auto print:px-0 print:mx-0 bg-white ${isLast ? 'pb-11' : 'mb-8'}`}
-    >
-      <h2 className="text-heading-1-mobile sm:text-heading-1 mb-6">{title}</h2>
-      {content && <p className="text-body-lg-mobile sm:text-body-lg mb-8">{content}</p>}
-      {children}
-    </div>
-  );
-};
-
-interface ContainerProps {
+interface ContentProps {
   className?: string;
   children?: React.ReactNode;
   /** For the css-classname */
   headingLevel?: 1 | 2;
   verticalPaddingClass?: string;
+  headingClassName?: string;
 }
 
 const Content = ({
   className = '',
+  headingClassName = '',
   title,
   children,
   headingLevel = 1,
   verticalPaddingClass = 'py-7 lg:py-8',
-}: ContainerProps & { title?: string }) => {
+}: ContentProps & { title?: string }) => {
   const headingClass = `sm:text-heading-${headingLevel} text-heading-${headingLevel}-mobile`;
 
   return (
@@ -223,8 +128,45 @@ const Content = ({
         className,
       ])}
     >
-      {title && <h2 className={`${headingClass} max-w-[716px]`}>{title}</h2>}
+      {title && <h2 className={`${headingClass} ${headingClassName} max-w-[716px]`}>{title}</h2>}
       {children}
+    </div>
+  );
+};
+
+interface HomeAccordionProps {
+  title: string;
+  content: string;
+  icon: React.ReactNode;
+  setIsOpen?: (isOpen: boolean) => void;
+  isOpen?: boolean;
+}
+const HomeAccordion = ({ title, content, icon, isOpen, setIsOpen }: HomeAccordionProps) => {
+  const triggerId = React.useId();
+  const contentId = `${triggerId}-panel`;
+  return (
+    <div className="shadow-[0_2px_8px_rgba(0,0,0,0.15)] bg-white py-3 px-4 flex items-center gap-4 w-full sm:max-w-[717px]">
+      <Accordion
+        ariaLabel={title}
+        triggerId={triggerId}
+        ariaControls={contentId}
+        initialState={false}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        title={
+          <div className="flex gap-4 items-center">
+            <div className="bg-secondary-gray size-7 flex items-center justify-center text-white rounded-full">
+              {icon}
+            </div>
+            {title}
+          </div>
+        }
+        className="w-full"
+      >
+        <section id={contentId} className="font-arial text-body-md-mobile sm:text-body-md ml-[44px] mb-6 mt-3">
+          {content}
+        </section>
+      </Accordion>
     </div>
   );
 };
@@ -284,15 +226,85 @@ const Home = () => {
     }
   }, [language, t]);
 
+  const [openAccordion, setOpenAccordion] = React.useState<1 | 2 | 3 | 4 | null>(null);
+
   return (
-    <main role="main" className="mx-auto w-full max-w-(--breakpoint-xl) bg-white" id="jod-main">
+    <main role="main" className="mx-auto w-full max-w-(--breakpoint-xl) bg-white pb-11" id="jod-main">
       <title>{t('common:osaamispolku')}</title>
       <MainCard />
       <Content title={t('home.welcome.title')} className="mt-7 lg:mt-8" verticalPaddingClass="pt-7 lg:pt-8">
         <p className="text-body-lg whitespace-pre-line max-w-[716px]">{t('home.welcome.content')}</p>
       </Content>
-      <Content title={t('home.competency-path-help.you.title')} headingLevel={2}>
-        <div className="text-body-lg whitespace-pre-line max-w-[716px] flex flex-col sm:gap-6 gap-5">
+      <Content title={t('home.competency-path-help.situation.title')} headingLevel={2}>
+        <div className="flex flex-col gap-3">
+          <HomeAccordion
+            isOpen={openAccordion === 1}
+            setIsOpen={() => setOpenAccordion(openAccordion === 1 ? null : 1)}
+            title={t('home.competency-path-help.situation.accordion-1-title')}
+            content={t('home.competency-path-help.situation.accordion-1-text')}
+            icon={<JodQuestionMark size={18} />}
+          />
+          <HomeAccordion
+            isOpen={openAccordion === 2}
+            setIsOpen={() => setOpenAccordion(openAccordion === 2 ? null : 2)}
+            title={t('home.competency-path-help.situation.accordion-2-title')}
+            content={t('home.competency-path-help.situation.accordion-2-text')}
+            icon={<JodBuild size={18} />}
+          />
+          <HomeAccordion
+            isOpen={openAccordion === 3}
+            setIsOpen={() => setOpenAccordion(openAccordion === 3 ? null : 3)}
+            title={t('home.competency-path-help.situation.accordion-3-title')}
+            content={t('home.competency-path-help.situation.accordion-3-text')}
+            icon={<JodCertificate size={18} />}
+          />
+          <HomeAccordion
+            isOpen={openAccordion === 4}
+            setIsOpen={() => setOpenAccordion(openAccordion === 4 ? null : 4)}
+            title={t('home.competency-path-help.situation.accordion-4-title')}
+            content={t('home.competency-path-help.situation.accordion-4-text')}
+            icon={<JodFavs size={18} />}
+          />
+        </div>
+      </Content>
+      <Content className="gap-7 grid lg:grid-cols-3 grid-cols-1">
+        <HeroCard
+          backgroundColor="var(--color-secondary-1-dark-2)"
+          content={t('home.sections.osaamispolku.content')}
+          title={t('home.sections.osaamispolku.title')}
+          size="sm"
+          titleLevel={2}
+          linkComponent={Link}
+          buttonLabel={t('home.sections.osaamispolku.link-text')}
+          buttonVariant="white"
+          to={`/yksilo/${language}`}
+          buttonIcon={<JodOpenInNew ariaLabel={t('common:external-link')} />}
+        />
+        <HeroCard
+          backgroundColor="var(--color-secondary-2-dark)"
+          content={t('home.sections.ohjaaja.content')}
+          title={t('home.sections.ohjaaja.title')}
+          size="sm"
+          titleLevel={2}
+          linkComponent={Link}
+          to={`/ohjaaja/${language}`}
+          buttonLabel={t('home.sections.ohjaaja.link-text')}
+          buttonIcon={<JodOpenInNew ariaLabel={t('common:external-link')} />}
+        />
+        <HeroCard
+          backgroundColor="var(--color-secondary-4-dark)"
+          content={t('home.sections.tietopalvelu.content')}
+          title={t('home.sections.tietopalvelu.title')}
+          to={`/tietopalvelu/${language}`}
+          size="sm"
+          titleLevel={2}
+          linkComponent={Link}
+          buttonLabel={t('home.sections.tietopalvelu.link-text')}
+          buttonIcon={<JodOpenInNew ariaLabel={t('common:external-link')} />}
+        />
+      </Content>
+      <Content title={t('home.competency-path-help.you.title')} headingLevel={2} headingClassName="max-w-[748px]!">
+        <div className="text-body-lg whitespace-pre-line max-w-[748px] flex flex-col sm:gap-6 gap-5">
           <p>{t('home.competency-path-help.you.text-1')}</p>
           <p>{t('home.competency-path-help.you.text-2')}</p>
         </div>
@@ -315,7 +327,6 @@ const Home = () => {
                 }}
               />
             </div>
-            <div>{t('home.competency-path-help.opintopolku.text-2')}</div>
           </div>
           <div className="flex flex-col gap-5 md:max-w-[320px]">
             <div className="h-[70px] flex flex-col justify-center items-start">{tmtLogo}</div>
@@ -335,7 +346,6 @@ const Home = () => {
                 }}
               />
             </div>
-            <div>{t('home.competency-path-help.tyomarkkinatori.text-2')}</div>
           </div>
           <div className="flex flex-col gap-5 md:max-w-[320px]">
             <div className="h-[70px] flex flex-col justify-center items-start">
@@ -360,77 +370,25 @@ const Home = () => {
                 }}
               />
             </div>
-            <div>{t('home.competency-path-help.opinfi.text-2')}</div>
           </div>
         </div>
       </Content>
-      <div className="mt-8">
-        <h2 className="mb-8 mx-auto max-w-[1092px] px-5 sm:px-6 xl:px-0 text-heading-2 sm:text-heading-1">
-          {t('home.sections.title')}
-        </h2>
-        <SecondaryCard
-          color="#004e82"
-          content={t('home.sections.osaamispolku.content')}
-          title={t('home.sections.osaamispolku.title')}
-          to={`/yksilo/${language}`}
-          buttonLabel={t('home.sections.osaamispolku.link-text')}
-          buttonIcon={<JodOpenInNew ariaLabel={t('common:external-link')} />}
-          bgImageClassName=" bg-[url(@/../assets/osaamispolkuni.jpg)] bg-cover bg-[50%_50%]"
-        />
-        <SecondaryCard
-          color="#00818A"
-          content={t('home.sections.ohjaaja.content')}
-          title={t('home.sections.ohjaaja.title')}
-          to={`/ohjaaja/${language}`}
-          buttonLabel={t('home.sections.ohjaaja.link-text')}
-          buttonIcon={<JodOpenInNew ariaLabel={t('common:external-link')} />}
-          bgImageClassName=" bg-[url(@/../assets/ohjaaja.jpg)] bg-cover bg-[50%_50%]"
-        />
-        <SecondaryCard
-          color="#AD4298"
-          content={t('home.sections.tietopalvelu.content')}
-          title={t('home.sections.tietopalvelu.title')}
-          to={`/tietopalvelu/${language}`}
-          buttonIcon={<JodOpenInNew ariaLabel={t('common:external-link')} />}
-          buttonLabel={t('home.sections.tietopalvelu.link-text')}
-          bgImageClassName="bg-[url(@/../assets/tietopalvelu.jpg)] bg-cover bg-[50%_50%]"
-        />
-      </div>
 
-      <Block title={t('home.palvelut.title')} isLast={true}>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-7 gap-y-7 sm:gap-y-11 mb-11 sm:mb-[176px]">
-          <ServiceBlock
-            title={t('home.palvelut.tool.title')}
-            text={t('home.palvelut.tool.content')}
-            buttonLabel={t('home.palvelut.tool.link-text')}
-            to={`/yksilo/${language}/${t('home.services.tool.path')}`}
-          />
-          <ServiceBlock
-            title={t('home.palvelut.ohjaus.title')}
-            text={t('home.palvelut.ohjaus.content')}
-            buttonLabel={t('home.palvelut.ohjaus.link-text')}
-            to={`/ohjaaja/${language}/${t('home.services.ohjaus.path')}`}
-            className="bg-secondary-2! active:bg-[#00889b]!"
-          />
-          <ServiceBlock
-            title={t('home.palvelut.tietopalvelu.title')}
-            text={t('home.palvelut.tietopalvelu.content')}
-            buttonLabel={t('home.palvelut.tietopalvelu.link-text')}
-            to={`/tietopalvelu/${language}/${t('home.palvelut.tietopalvelu.path')}`}
-            className="bg-secondary-4! active:bg-[#ad3ea3]!"
+      <Content className="mb-11">
+        <div className="max-w-[716px] mb-11">
+          <HeroCard
+            content={t('home.personal-guidance.content')}
+            title={t('home.personal-guidance.title')}
+            to={t('common:navigation.extra.palveluhakemisto.url')}
+            linkComponent={Link}
+            buttonLabel={t('home.personal-guidance.link-text')}
+            buttonIcon={<JodOpenInNew ariaLabel={t('common:external-link')} />}
+            titleLevel={2}
+            titleClassName="text-heading-2"
+            backgroundColor="var(--color-secondary-2-dark)"
           />
         </div>
-      </Block>
-
-      <SecondaryCard
-        color="#00818A"
-        content={t('home.personal-guidance.content')}
-        title={t('home.personal-guidance.title')}
-        to={t('common:navigation.extra.palveluhakemisto.url')}
-        buttonLabel={t('home.personal-guidance.link-text')}
-        buttonIcon={<JodOpenInNew ariaLabel={t('common:external-link')} />}
-        bgImageClassName=" bg-[url(@/../assets/palveluhakemisto.jpg)] bg-cover bg-[50%_50%]"
-      />
+      </Content>
     </main>
   );
 };
