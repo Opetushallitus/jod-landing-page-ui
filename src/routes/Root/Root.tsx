ec85af6a557802f31efbaeec8ebfbc27de036fa6
Juhani Koivulainen <juhani.koivulainen@elenium.fi>
2026-03-11 10:39:21 +0200

    OPHJOD-3202: Update DS and add social media links

--- src/routes/Root/Root.tsx
+++ src/routes/Root/Root.tsx
@@ -65,6 +65,21 @@ const Root = () => {
     },
   ];
 
+  const socialMedia: React.ComponentProps<typeof Footer>['socialMedia'] = {
+    facebook: {
+      href: 'https://www.facebook.com/osaamispolku',
+      label: t('common:footer.social-media.facebook'),
+    },
+    instagram: {
+      href: 'https://www.instagram.com/osaamispolku/',
+      label: t('common:footer.social-media.instagram'),
+    },
+    linkedin: {
+      href: 'https://www.linkedin.com/company/osaamispolku',
+      label: t('common:footer.social-media.linkedin'),
+    },
+  };
+
   React.useEffect(() => {
     document.documentElement.setAttribute('lang', language);
   }, [language]);
@@ -153,6 +168,7 @@ const Root = () => {
         feedbackOnClick={() => setFeedbackVisible(true)}
         feedbackBgImageClassName="bg-[url(@/../assets/feedback.jpg)] bg-cover bg-[50%_50%]"
         copyright={t('common:footer.copyright')}
+        socialMedia={socialMedia}
         externalLinkIconAriaLabel={t('common:external-link')}
         testId="footer"
       />
