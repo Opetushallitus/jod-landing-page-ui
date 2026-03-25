5fd5a940b6fc599707e813982be4ab6f94ffbc7a
Sauli Anto <sauli.anto@gofore.com>
2026-03-25 10:47:53 +0200

    OPHJOD-3191: Add HowToUse component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { FeedbackModal } from './FeedbackModal/FeedbackModal';
+export { HowToUse } from './HowToUse/HowToUse';
 export { MainLayout } from './MainLayout/MainLayout';
 export { UserButton } from './UserButton/UserButton';
