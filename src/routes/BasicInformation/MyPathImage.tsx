39d70a792d92ae0e959145438d8471b9a7ce5067
Sauli Anto <sauli.anto@gofore.com>
2026-07-30 14:51:38 +0300

    OPHJOD-851: Update dependencies

--- src/routes/BasicInformation/MyPathImage.tsx
+++ src/routes/BasicInformation/MyPathImage.tsx
@@ -24,19 +24,19 @@ export const getLocalizedLogos = (language: string) => {
   switch (language) {
     case 'sv':
       return {
-        osaamispolkuLogo: <LogoRgbSv size="26" />,
+        osaamispolkuLogo: <LogoRgbSv size={26} />,
         opintopolkuLogo: <LogoOpintopolkuSv height={20} />,
         tyomarkkinatoriLogo: <LogoTyomarkkinatoriSv height={20} />,
       };
     case 'en':
       return {
-        osaamispolkuLogo: <LogoRgbEn size="26" />,
+        osaamispolkuLogo: <LogoRgbEn size={26} />,
         opintopolkuLogo: <LogoOpintopolkuEn height={20} />,
         tyomarkkinatoriLogo: <LogoTyomarkkinatoriEn height={20} />,
       };
     default:
       return {
-        osaamispolkuLogo: <LogoRgbFi size="26" />,
+        osaamispolkuLogo: <LogoRgbFi size={26} />,
         opintopolkuLogo: <LogoOpintopolkuFi height={20} />,
         tyomarkkinatoriLogo: <LogoTyomarkkinatoriFi height={20} />,
       };
